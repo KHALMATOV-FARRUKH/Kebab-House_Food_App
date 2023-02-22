02f2cc9a267cb671642f66cdf2347f14f3d2ad8a
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2023-02-22 23:27:09 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/appbiz/flutter_grocery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.appbiz.flutter_grocery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/appbiz/shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.appbiz.shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
